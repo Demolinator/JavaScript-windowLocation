d4daac86c351140f7af1fae0527bfb8d58c9bc18
Muhammad Fareed <mfareed1504@gmail.com>
2025-09-08 10:21:38 +0500

    hash added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //  var whereWeAt = window.location.href;
 
-var pathToFile = location.pathname;
-var domainName = location.hostname;
- console.log(pathToFile, domainName);
+var pathToFile = window.location.pathname;
+var domainName = window.location.hostname;
+var hashName = window.location.hash
+ console.log(pathToFile, domainName, hashName);
